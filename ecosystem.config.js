6197b61ea7c05a9c17f5f7845634021fbf003dc3
ricks140488 <rdominiud@gmail.com>
2024-08-15 00:46:18 -0700

    edit footer

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./server.js",
+env : {
+NODE_ENV : "production",
+NEXT_PUBLIC_BASE_URL : "https://admin,ecobiz.asia/"
+}
+  }]
+}
